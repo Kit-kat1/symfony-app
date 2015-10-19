1b896181497bc1da09b38dedd77877f182b98193
Kit-kat1 <Katynichka95@mail.ru>
2015-10-19 23:05:22 +0300

    edit fosuserbundle

--- src/AppBundle/Controller/AuthController.php
+++ src/AppBundle/Controller/AuthController.php
@@ -17,26 +17,26 @@ use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class AuthController extends Controller
 {
-    /**
-     * @Route("/login")
-     */
-    public function loginAction()
-    {
-        if ($this->get('request')->attributes->has(Security::AUTHENTICATION_ERROR)) {
-            $error = $this->get('request')->attributes->get(Security::AUTHENTICATION_ERROR);
-        } else {
-            $error = $this->get('request')->getSession()->get(Security::AUTHENTICATION_ERROR);
-        }
-
-        return $this->render('admin2/login.html.twig', array(
-            'last_username' => $this->get('request')->getSession()->get(Security::LAST_USERNAME),
-            'error' => $error
-        ));
-    }
-    /**
-     * @Route("/login_check")
-     */
-    public function loginCheckAction()
-    {
-    }
+//    /**
+//     * @Route("/login")
+//     */
+//    public function loginAction()
+//    {
+//        if ($this->get('request')->attributes->has(Security::AUTHENTICATION_ERROR)) {
+//            $error = $this->get('request')->attributes->get(Security::AUTHENTICATION_ERROR);
+//        } else {
+//            $error = $this->get('request')->getSession()->get(Security::AUTHENTICATION_ERROR);
+//        }
+//
+//        return $this->render('admin2/login.html.twig', array(
+//            'last_username' => $this->get('request')->getSession()->get(Security::LAST_USERNAME),
+//            'error' => $error
+//        ));
+//    }
+//    /**
+//     * @Route("/login_check")
+//     */
+//    public function loginCheckAction()
+//    {
+//    }
 }
